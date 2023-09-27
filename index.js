921474e4cd9e7a29383f9eb15a9726b720364804
Neematrashid <nymatrasheed05@gmail.com>
2023-09-27 21:39:39 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-// Write your code here!
+const element = document.createElement("div");
+document.body.append(element);
+
+//const newHeader = document.createElement(h1);
+const newHeader = document.createElement('h1#victory')
+newHeader.id = 'victory';
+
+const Yourname = 'Neemat Rashid';
+newHeader.textContent = `${Yourname} is the champion`;
+
+const body = document.querySelector('body');
+
+body.appendChild(newHeader);
+
+//document.body.appendChild(newHeader);
