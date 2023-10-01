d372a057cf7c12feb6f192be61946b06f17aee7a
Neematrashid <nymatrasheed05@gmail.com>
2023-10-01 06:28:12 +0300

    complete updated assignment

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
-const element = document.createElement("div");
-document.body.append(element);
 
-//const newHeader = document.createElement(h1);
-const newHeader = document.createElement('h1#victory')
+
+const body = document.querySelector('body');
+const newHeader = document.createElement('h1');
 newHeader.id = 'victory';
 
 const Yourname = 'Neemat Rashid';
 newHeader.textContent = `${Yourname} is the champion`;
 
-const body = document.querySelector('body');
 
 body.appendChild(newHeader);
 
